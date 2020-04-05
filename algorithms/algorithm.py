ad46b38da615f9625a85e98bf545ef88d1b5f97b
Yabk <yabk1010@gmail.com>
2020-04-05 16:59:19 +0200

    Add save_best_individual algorithm method

--- algorithms/algorithm.py
+++ algorithms/algorithm.py
@@ -1,5 +1,6 @@
 """Module containing learning algorithm abstract class"""
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 
 class Algorithm(ABC):
@@ -7,7 +8,13 @@ class Algorithm(ABC):
 
     def __init__(self):
         """Initialize hyperparameters for the algorithm"""
+        self.best_individual = None
 
     @abstractmethod
     def run(self):
         """Run the algorithm"""
+
+    def save_best_individual(self):
+        """Save the best individual to file"""
+        filename = datetime.today().isoformat()+'-'+self.__class__.__name__+'.txt'
+        self.best_individual.to_file('./best_individuals/'+filename)
