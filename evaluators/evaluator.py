7e8eace0f80d86ac79cbaf8fe26d501ebc69b479
Yabk <yabk1010@gmail.com>
2020-04-06 19:37:22 +0200

    Modify Evaluator abstract class

--- evaluators/evaluator.py
+++ evaluators/evaluator.py
@@ -5,15 +5,17 @@ from abc import ABC, abstractmethod
 class Evaluator(ABC):
     """Abstract evaluator class"""
 
-    def __init__(self):
+    def __init__(self, input_len, output_len):
         """Initialize the evaluator"""
-        self.input_len = 0
-        self.output_len = 0
+        self.input_len = input_len
+        self.output_len = output_len
 
     @abstractmethod
     def evaluate(self, individual):
         """Evaluate a single individual"""
 
-    @abstractmethod
     def batch_evaluate(self, individuals):
         """Evaluate a batch of individuals and sort them by fitness in descending order"""
+        for individual in range(len(individuals)):
+            self.evaluate(individual)
+        individuals.sort(key=lambda individual: individual.fitness, reverse=True)
