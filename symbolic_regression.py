566ebe7c12b991c7eb73edb19bf70f0076fd945a
Yabk <yabk1010@gmail.com>
2020-04-25 21:52:07 +0200

    Fix typo

--- symbolic_regression.py
+++ symbolic_regression.py
@@ -1,4 +1,4 @@
-"""Module contianing a test run of Genetic Algorithm and CGP in problem of symbolic regression"""
+"""Module contianing a test run of Evolution Strategy and CGP in problem of symbolic regression"""
 from algorithms.evolution_strategy import EvolutionStrategy
 from reporters.best_individual_reporter import BestIndividualReporter
 from evaluators.math_function_evaluator import MathFunctionEvaluator
@@ -7,7 +7,7 @@ from genotypes.cgp.cgp import CGPGenerator
 
 
 def main():
-    """Run a symbolic regression on "x+y-z" expression using CGP and genetic algorithm"""
+    """Run a symbolic regression on "x+y-z" expression using CGP and symbolic regression"""
     evaluator = MathFunctionEvaluator('x-8*y-z', [(-1, -1, 5), (-2, -3, 2), (0, -1, -123),
                                                   (1, 3, 5), (-2, -1, 0), (2, 3, 2)])
     reporters = [BestIndividualReporter()]
